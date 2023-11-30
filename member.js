2e23fae732e3cac6461df04643b68cf201e82c58
maxsaintadjutor <149595828+maxsaintadjutor@users.noreply.github.com>
2023-11-30 07:36:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,60 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "image": "img/html.png",
+            "description": "HTML is the standard markup language for creating Web pages."
+        },
+        {
+            "name": "CSS",
+            "image": "img/css.png",
+            "description": "CSS is a language that describes the style of an HTML document."
+        },
+        {
+            "name": "JavaScript",
+            "image": "img/js.png",
+            "description": "JavaScript is the programming language of HTML and the Web."
+        },
+        {
+            "name": "PHP",
+            "image": "img/php.png",
+            "description": "PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages."
+        },
+        {
+            "name": "MySQL",
+            "image": "img/mysql.png",
+            "description": "MySQL is the world's most popular open source database."
+        },
+        {
+            "name": "Bootstrap",
+            "image": "img/bootstrap.png",
+            "description": "Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web."
+        },
+        {
+            "name": "jQuery",
+            "image": "img/jquery.png",
+            "description": "jQuery is a fast, small, and feature-rich JavaScript library."
+        },
+        {
+            "name": "AngularJS",
+            "image": "img/angularjs.png",
+            "description": "AngularJS is a structural framework for dynamic web apps."
+        },
+        {
+            "name": "Laravel",
+            "image": "img/laravel.png",
+            "description": "Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern."
+        },
+        {
+            "name": "CodeIgniter",
+            "image": "img/codeigniter.png",
+            "description": "CodeIgniter is an open-source software rapid development web framework, for use in building dynamic web sites with PHP."
+        },
+        {
+            "name": "Git",
+            "image": "img/git.png",
+            "description": "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency."
+        },
+        {
+            "name": "GitHub",
+            "image": "
